20414c29d62f1846d01685f0613f1aa42ceaa061
Tangshengbo <867850520tangbo@sina.com>
2018-11-22 10:52:12 +0800

    add proxy code

--- src/main/java/com/tangshengbo/design/proxy/log/CheckLogProxy.java
+++ src/main/java/com/tangshengbo/design/proxy/log/CheckLogProxy.java
@@ -8,24 +8,37 @@ import java.util.Arrays;
 /**
  * Created by TangShengBo on 2017-09-12.
  */
-public class CheckLogProxy implements InvocationHandler {
+public class CheckLogProxy {
 
     private Object target;
 
     public Object getInstance(Object target) {
         this.target = target;
         Class targetClass = this.target.getClass();
-        return Proxy.newProxyInstance(targetClass.getClassLoader(), targetClass.getInterfaces(), this);
+        return Proxy.newProxyInstance(targetClass.getClassLoader(),
+                targetClass.getInterfaces(), new MyInvocationHandler(target));
     }
 
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.println(proxy.getClass().getName());
-        long starTime = System.currentTimeMillis();
-        System.out.println("当前方法名:" + method.getName() + "参数:" + Arrays.toString(args));
-        Object result = method.invoke(target, args);
-        long endTime = System.currentTimeMillis();
-        System.out.println("方法返回值:" + result + "耗时:" + (endTime - starTime) / 1000.0 + "s");
-        return result;
+    /**
+     * 回调拦截器
+     */
+    private static class MyInvocationHandler implements InvocationHandler {
+
+        private Object target;
+
+        MyInvocationHandler(Object target) {
+            this.target = target;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println(proxy.getClass().getName());
+            long starTime = System.currentTimeMillis();
+            System.out.println("当前方法名:" + method.getName() + "参数:" + Arrays.toString(args));
+            Object result = method.invoke(target, args);
+            long endTime = System.currentTimeMillis();
+            System.out.println("方法返回值:" + result + "耗时:" + (endTime - starTime) / 1000.0 + "s");
+            return result;
+        }
     }
 }
